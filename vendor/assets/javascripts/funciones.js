75853ece5877866967ff7a478edb047eb4539520
valde1992 <fernando_valde@hotmail.com>
2015-06-10 18:11:57 -0300

    Funciones actualizadas

--- vendor/assets/javascripts/funciones.js
+++ vendor/assets/javascripts/funciones.js
@@ -1,40 +1,53 @@
-<script>
+//<script>
 	function ValidarFecha(fecha){
 		var f1 = new Date(fecha);
-		var f2 = new Date(getDate());
-		ff1 = parse(f1);
-		ff2 = parse (f2);
-		if (ff1 > ff2){
+		var f2 = new Date();
+		ff1 = Date.parse(f1);
+		ff2 = Date.parse(f2);
+		if (ff1 < ff2){
 			text = "Fecha invalida";
 			return false;
 		}
 		else {
+			alert('izi');
 			return true;
 		}
 	}
 
-	function ValidarNombre(nombre){
-		
-		
-		
-	}
-
-	function ValidarNumero(x){
-		if(x<0||x>51){
-			text= "Numero Invalido"
+function ValidarNombre(nombre){
+		var regex = /^[a-zA-Z ]{2,30}$/;
+		var ctrl = nombre;
+		if (regex.test(ctrl)) {
+			alert('Nombre Correcto');
+			return true;
+		}
+		else {
+			alert('izi');
 			return false;
-		else{
-			return true;
-		}
+		}	
 	}
-	function ValidarContr(c1,c2){
-		if(c1==c2){
+
+function ValidarNumero(x){
+		
+		var n=parseInt(x);
+		if(n>0 && n<51){
+			alert('Mozo enviado a la Mesa Numero:  ' +  n);
 			return true;
 		}
-		else{
-			text= "Tienen que coincidir las contrasenias";
-		return false;
+		else{ 
+			alert('Caracter o Valor ingresado invalido');
+			return false;
 		}
 	}
 	
-</script>
+	function ValidarContr(c1,c2){
+		if(c1==c2){
+			alert('izi');
+			return true;
+		}
+		else{
+			alert('Tienen que coincidir las contrasenias');
+		return false;
+		}
+	}
+//</script>
